f54f1671487e5c1ab76936c8e889c0533e796c4d
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-04-12 16:32:11 +0200

    contract: make create_good external

--- Contracts/v0.1/contracts/givo_mock.sol
+++ Contracts/v0.1/contracts/givo_mock.sol
@@ -28,7 +28,7 @@ contract givo {
   mapping (uint => Good[]) public offers;
   mapping (address => uint) address_to_id;
 
-  function create_good(uint id, string memory name, string memory ipfs_image, string memory ipfs_details) internal returns (bool) {
+  function create_good(uint id, string memory name, string memory ipfs_image, string memory ipfs_details) external returns (bool) {
     return true;
   }
 }

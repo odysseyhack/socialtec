87270eeeb410fed1a0498543d7685efe77d6b432
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-04-12 16:28:57 +0200

    contract: add mock contract and update contract name

---
+++ Contracts/v0.1/contracts/givo_mock.sol
@@ -0,0 +1,34 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+pragma experimental ABIEncoderV2;
+
+contract givo {
+
+  struct Good {
+    address node;
+    string name;
+    string ipfs_image;
+    string ipfs_details;
+  }
+
+  uint good_count = 5;
+
+  struct Node {
+    address addr;
+    Message[] mail_box;
+  }
+
+  struct Message {
+    address from;
+    address refer;
+    string body;
+  }
+
+  Node[] nodes;
+  mapping (uint => Good[]) public offers;
+  mapping (address => uint) address_to_id;
+
+  function create_good(uint id, string memory name, string memory ipfs_image, string memory ipfs_details) internal returns (bool) {
+    return true;
+  }
+}

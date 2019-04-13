ab5484a2e5055028ba5e9f8f6c0f6ca4037d6f1b
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-04-13 10:27:03 +0200

    contract: add js for api and remove unwanted contract

--- node/gui/App/assets/js/main.js
+++ node/gui/App/assets/js/main.js
@@ -3,18 +3,22 @@
     // Document Ready
     $(document).ready(function() {
 
-        var swiper = new Swiper('.swiper-container', {
-          slidesPerView: 'auto',
-          centeredSlides: true,
-          spaceBetween: 0,
-          grabCursor: true,
-        });
+      $.get( "http://6ec69753.ngrok.io/offers", function(data, status){
+
+        for(var cntr = 0;cntr<data.length; cntr++){
+          var card = '<div class="offer swiper-slide"><div class="offer__content" style="background-image: url('+data[cntr]['image_url']+');"><div class="offer__body"><h3 class="offer__title">'+data[cntr]['name']+'</h3><small class="offer__user">by Elit Numquam</small><p>'+data[cntr]['details']+'</p></div><div class="offer__meta"><ul><li><strong>4</strong>Likes</li><li><strong>11</strong>Trades</li><li><strong>680</strong>Respect</li></ul></div></div></div>'
+          $( ".swiper-wrapper" ).append(card);
+        }
+
+      },"json");
+
+      var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 0,
+        grabCursor: true,
+      });
 
     });
 
-
-    // Window Load
-    $(window).load(function() {
-    });
-
-})(jQuery); 
+})(jQuery);

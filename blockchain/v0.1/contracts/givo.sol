0a1d52449ee0a40d4e3b24e40207e5258a0253c1
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-04-13 13:10:31 +0200

    contract: modify the good storag

--- blockchain/v0.1/contracts/givo.sol
+++ blockchain/v0.1/contracts/givo.sol
@@ -5,17 +5,17 @@ pragma solidity >=0.4.0 <0.7.0;
 contract givo {
 
   struct Good {
-    address node;
+    uint node;
     string name;
     string ipfs_image;
     string ipfs_details;
   }
 
   uint constant good_count = 5;
-  uint constant page_size = 5;
   uint public node_cntr = 0;
-  mapping (uint => Good[]) public offers;
+  mapping (uint => uint[]) public offers;
   mapping (address => uint) address_to_id;
+  Good[] public all_goods;
 
   event intrested(uint to, uint good_id, uint from);
   event not_intrested(uint to, uint good_id, uint from);
@@ -23,58 +23,48 @@ contract givo {
   event deleted(uint owner, uint good_id);
   event chained(uint to, uint get_owner, uint get_good_id, uint give_owner, uint give_good_id);
 
-  function create_offer(string memory name, string memory ipfs_image, string memory ipfs_details) public returns (bool) {
-    Good[] storage my_offers = offers[address_to_id[msg.sender]];
+  function create_offer(string memory name, string memory ipfs_image, string memory ipfs_details) public returns (uint good_id) {
+    uint[] storage my_offers = offers[address_to_id[msg.sender]];
     if(my_offers.length==0){
         address_to_id[msg.sender] = node_cntr;
         node_cntr++;
     }
-    Good memory new_good = Good(msg.sender, name, ipfs_image, ipfs_details);
-    my_offers.push(new_good);
-    return true;
+    Good memory new_good = Good(address_to_id[msg.sender], name, ipfs_image, ipfs_details);
+    good_id = all_goods.push(new_good)-1;
+    my_offers.push(good_id);
+    return good_id;
   }
 
   function delete_offer(uint good_id) public{
-    Good[] storage my_offers = offers[address_to_id[msg.sender]];
+    uint[] storage my_offers = offers[address_to_id[msg.sender]];
     require(my_offers.length>=good_id);
     my_offers[good_id] = my_offers[my_offers.length-1];
     my_offers.pop();
     emit deleted(address_to_id[msg.sender], good_id);
   }
 
-/*  function get_offers(uint page) public view returns (Good[page_size][good_count] memory goods){
-      uint last_visit = (page-1)*page_size;
-      require(last_visit<node_cntr);
-      uint max_nodes = page_size;
-      if(page*page_size>node_cntr){
-          max_nodes = node_cntr - last_visit;
-      }
+  function get_offers(uint node_id) public view returns (uint[] memory my_offers){
+      my_offers = offers[node_id];
+      return my_offers;
+  }
 
-      for(uint node_cntr = 0; node_cntr<max_nodes; node_cntr++){
-          for(uint good_cntr = 0; good_cntr<good_count; good_cntr++){
-              goods[node_cntr][good_cntr] = new Good(msg.sender,"a","b","c");
-             //goods[node_cntr][good_cntr] = offers[last_visit+node_cntr][good_cntr];
-          }
-      }
-      return goods;
-  }*/
-
-  function add_intrest(uint node_id, uint good_id) public {
+  function add_intrest(uint good_id) public {
+      uint node_id = all_goods[good_id].node;
       emit intrested(node_id, good_id, address_to_id[msg.sender]);
   }
 
-  function delete_intrest(uint node_id, uint good_id) public {
+  function delete_intrest(uint good_id) public {
+      uint node_id = all_goods[good_id].node;
       emit intrested(node_id, good_id, address_to_id[msg.sender]);
   }
 
-  function refer_intrest(uint node_id, uint good_id, uint refer_id) public {
+  function refer_intrest(uint good_id, uint refer_id) public {
+      uint node_id = all_goods[good_id].node;
       emit refer(node_id, good_id, address_to_id[msg.sender], refer_id);
   }
 
   function cycle_formed(uint to, uint get_good_id, uint give_owner, uint give_good_id) public{
       emit chained(to, address_to_id[msg.sender], get_good_id, give_owner, give_good_id);
   }
-
-
 
 }
--- node/gui/App/assets/js/main_search.js
+++ node/gui/App/assets/js/main_search.js
@@ -1,3 +1,7 @@
+function item_liked(x) {
+  console.log(x);
+}
+
 (function($) {
 
     // Document Ready
@@ -6,7 +10,7 @@
       $.get( "http://6ec69753.ngrok.io/offers", function(data, status){
 
         for(var cntr = 0;cntr<data.length; cntr++){
-          var card = '<div class="offer swiper-slide"><div class="offer__content" style="background-image: url('+data[cntr]['image_url']+');"><div class="offer__body"><h3 class="offer__title">'+data[cntr]['name']+'</h3><small class="offer__user">by Elit Numquam</small><p>'+data[cntr]['details']+'</p></div><div class="offer__meta"><ul><li><strong>4</strong>Likes</li><li><strong>11</strong>Trades</li><li><strong>680</strong>Respect</li></ul><button class="btn-interested"><span>+</span></button></div></div></div>'
+          var card = '<div class="offer swiper-slide"><div class="offer__content" style="background-image: url('+data[cntr]['image_url']+');"><div class="offer__body"><h3 class="offer__title">'+data[cntr]['name']+'</h3><small class="offer__user">by Elit Numquam</small><p>'+data[cntr]['details']+'</p></div><div class="offer__meta"><ul><li><strong>4</strong>Likes</li><li><strong>11</strong>Trades</li><li><strong>680</strong>Respect</li></ul><button class="btn-interested" id="'+cntr+'" onclick="item_liked(this.id)><span>+</span></button></div></div></div>'
           $( ".swiper-wrapper" ).append(card);
         }
 

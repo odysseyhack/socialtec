413f05cdfb97e95a6b596c658722ac5b429f622a
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-04-13 11:46:38 +0200

    GUI: add buttons

--- node/gui/App/assets/js/main_search.js
+++ node/gui/App/assets/js/main_search.js
@@ -6,19 +6,19 @@
       $.get( "http://6ec69753.ngrok.io/offers", function(data, status){
 
         for(var cntr = 0;cntr<data.length; cntr++){
-          var card = '<div class="offer swiper-slide"><div class="offer__content" style="background-image: url('+data[cntr]['image_url']+');"><div class="offer__body"><h3 class="offer__title">'+data[cntr]['name']+'</h3><small class="offer__user">by Elit Numquam</small><p>'+data[cntr]['details']+'</p></div><div class="offer__meta"><ul><li><strong>4</strong>Likes</li><li><strong>11</strong>Trades</li><li><strong>680</strong>Respect</li></ul></div></div></div>'
+          var card = '<div class="offer swiper-slide"><div class="offer__content" style="background-image: url('+data[cntr]['image_url']+');"><div class="offer__body"><h3 class="offer__title">'+data[cntr]['name']+'</h3><small class="offer__user">by Elit Numquam</small><p>'+data[cntr]['details']+'</p></div><div class="offer__meta"><ul><li><strong>4</strong>Likes</li><li><strong>11</strong>Trades</li><li><strong>680</strong>Respect</li></ul><button class="btn-interested"><span>+</span></button></div></div></div>'
           $( ".swiper-wrapper" ).append(card);
         }
 
       },"json");
 
-      var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        spaceBetween: 0,
-        grabCursor: true,
-      });
 
     });
 
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 0,
+      grabCursor: true,
+    });
 })(jQuery);

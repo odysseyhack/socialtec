1e9cd7a83a90beef2bc4878395a1451e7bfd7874
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-04-19 12:19:48 +0200

    contracts: update givo.sol

--- contracts/givo.sol
+++ contracts/givo.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.4.0 <0.7.0;
 
 
-contract givo {
 
+contract givo {
   struct Good {
     uint node;
     uint index;
@@ -13,30 +13,41 @@ contract givo {
   }
 
   uint constant good_count = 5;
-  uint public node_cntr = 0;
+  uint public node_cntr = 1;
   uint public max_all_good =0;
   mapping (uint => uint[]) public offers;
-  mapping (address => uint) address_to_id;
+  mapping (address => uint) public address_to_id;
   Good[] public all_goods;
 
   event intrested(uint to, uint good_id, uint from);
   event not_intrested(uint to, uint good_id, uint from);
   event refer(uint to, uint good_id, uint from, uint refer_id);
   event deleted(uint owner, uint good_id);
-  event chained(uint to, uint get_owner, uint get_good_id, uint give_owner, uint give_good_id);
+  event chained(uint if_owner, uint if_good, uint if_receiver, uint then_good, uint then_receiver);
 
   function create_offer(string memory name, string memory ipfs_image, string memory ipfs_details) public returns (uint good_id) {
-    uint[] storage my_offers = offers[address_to_id[msg.sender]];
-    if(my_offers.length==0){
+    uint x = address_to_id[msg.sender];
+    if (x == 0) {
         address_to_id[msg.sender] = node_cntr;
-        node_cntr++;
+        node_cntr=node_cntr+1;
     }
+    uint[] storage my_offers = offers[address_to_id[msg.sender]];
     Good memory new_good = Good(address_to_id[msg.sender], 0, true, name, ipfs_image, ipfs_details);
     good_id = all_goods.push(new_good)-1;
     uint index = my_offers.push(good_id) -1;
     new_good.index = index;
     max_all_good = good_id;
     return good_id;
+  }
+
+  function get_id() public returns (uint) {
+    uint x = address_to_id[msg.sender];
+    if (x == 0) {
+        address_to_id[msg.sender] = node_cntr;
+        x = node_cntr;
+        node_cntr=node_cntr+1;
+    }
+    return x;
   }
 
   function delete_offer(uint good_id) public{
@@ -72,16 +83,12 @@ contract givo {
       emit refer(node_id, good_id, address_to_id[msg.sender], refer_id);
   }
 
-  function cycle_formed(uint to, uint get_good_id, uint give_owner, uint give_good_id) public{
-      emit chained(to, address_to_id[msg.sender], get_good_id, give_owner, give_good_id);
+  function cycle_formed(uint if_owner, uint if_good,  uint then_good, uint then_receiver) public{
+      emit chained(if_owner, if_good, address_to_id[msg.sender], then_good,then_receiver );
   }
-
-  //I give (good) (to) you if (owner) gives (con_good) to (getter).
-  function cycle_propagate(uint to, uint good, uint owner, uint con_good, uint getter) public{
-      emit chained(to, getter, good, owner, con_good);
+  
+  function cycle_propagate(uint if_owner, uint if_good,uint if_receiver,  uint then_good, uint then_receiver) public{
+      emit chained(if_owner, if_good, if_receiver, then_good,then_receiver );
   }
-
-
 
 }
-

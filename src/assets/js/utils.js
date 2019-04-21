c5cd3cc9ca476aecbf724d769d17c66a4716aafb
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-04-21 18:17:49 +0200

    js: add promiseify and in_my_interest()

---
+++ src/assets/js/utils.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 8 */
+
+promisify= (inner) =>
+    new Promise((resolve, reject) =>
+        inner((err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    );
+
+function in_my_interest(offers) {
+  my_interests = Storage.get_my_interests();
+  common = my_interests.filter(value => -1 !== offers.indexOf(value));
+  return common;
+}
